4e51a9ff35d4e198ca4073a305a940ee2c309a89
Nurgazy Nazhimidinov <nurgasemetey@gmail.com>
2019-12-04 11:51:30 +0300

    update output in table format

--- folder-categorizer.py
+++ folder-categorizer.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python
-from colorama import Fore, Back, Style
 import os, sys
-pathDirectory = sys.argv[1]
+from prettytable import PrettyTable
+import itertools
 from datetime import datetime
+
+pathDirectory = sys.argv[1]
 now = datetime.now()
 timestamp = datetime.timestamp(now)
 
 from pathlib import Path
-time_categories = [
-    {"timeframe": "recent", "color" : Fore.GREEN},
-    {"timeframe": "week_ago", "color" : Fore.YELLOW},
-    {"timeframe": "month_ago", "color" : Fore.MAGENTA},
-    {"timeframe": "six_months_ago", "color" : Fore.RED}
-    ]
+
 week_threshold = 3600*24*7;
 month_threshold = 3600*24*30;
 six_months_threshold = 3600*24*30*6;
@@ -27,22 +24,29 @@ folder_categories = {
 for path in Path(pathDirectory).iterdir():
     info = path.stat()
     week_ago = timestamp - week_threshold
-    
+    filename = os.path.basename(str(path))
     if(week_ago < info.st_mtime):
-        folder_categories["recent"].append(str(path))
+        folder_categories["recent"].append(str(filename))
         continue
     month_ago = timestamp - month_threshold
     if(month_ago < info.st_mtime):
-        folder_categories["week_ago"].append(str(path))
+        folder_categories["week_ago"].append(str(filename))
         continue
     six_months_ago = timestamp - six_months_threshold
     if(six_months_ago < info.st_mtime):
-        folder_categories["month_ago"].append(str(path))
+        folder_categories["month_ago"].append(str(filename))
         continue
-    folder_categories["six_months_ago"].append(str(path))
-for time_category in time_categories:
-    print(time_category["color"] + "--------------------------------------------------------------------")
-    print(time_category["timeframe"].upper())
-    for folder_category in folder_categories[time_category["timeframe"]]:
-        print(os.path.basename(folder_category))
-    print("--------------------------------------------------------------------")
+    folder_categories["six_months_ago"].append(str(filename))
+
+
+
+t = PrettyTable()
+titles = ['recet','week_ago', 'month_ago', 'six_months_ago']
+t.field_names = titles
+recent = folder_categories["recent"]
+week_ago = folder_categories["week_ago"]
+month_ago = folder_categories["month_ago"]
+six_months_ago = folder_categories["six_months_ago"]
+for row in reversed(list(itertools.zip_longest(recent, week_ago, month_ago, six_months_ago, fillvalue=""))):
+    t.add_row(list(row))
+print(t)
